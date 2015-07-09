8f3e1e96c95d21cd9e1b7671188d2c679e78a9c6
Alex Kaufman <alexkaufman06@gmail.com>
2015-07-09 14:18:24 -0700

    Add radial gradient for background

--- js/controllers/canvas.js
+++ js/controllers/canvas.js
@@ -3,7 +3,7 @@ angularArt.controller('CanvasCtrl', function CanvasCtrl($scope) {
                     sepia: 0, invert: 0, rotate: 345, width: 153, positionX: 211, positionY: 189,
                     scaleX: 1, scaleY: 1, border: 0 };
   $scope.canvas = { url: "http://www.psdgraphics.com/wp-content/uploads/2011/06/curved-road.jpg",
-            color: "#0080c0", sepia: 0, invert: 0, saturation: 100, hue: 0 };
+            color: "#0080c0", sepia: 0, invert: 0, saturation: 100, hue: 0, radialColor: "" };
   $scope.logo = { url: "http://ukartsdirectory.com/wp-content/uploads/2013/11/your-logo-here.png",
           width: 250, positionX: -253, positionY: -38, rotate: 0, border: 0, scaleX: 1,
           scaleY: 1 };
