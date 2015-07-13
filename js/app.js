48b98424cf15cd75a1b5fea3eaf4063d2509d8b8
Alex Kaufman <alexkaufman06@gmail.com>
2015-07-13 14:51:45 -0700

    Add glyphicons and custom aside for sepia

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-var angularArt = angular.module('angularArt', ['ui.bootstrap', 'ngRoute', 'ngAnimate', 'mgcrea.ngStrap']);
+var angularArt = angular.module('angularArt', ['ui.bootstrap', 'ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
