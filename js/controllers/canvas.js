b4bc9bd0d2752922e347373f4b2b9f9f1a413b97
Alex Kaufman <alexkaufman06@gmail.com>
2015-07-13 16:15:53 -0700

    Add more asides

--- js/controllers/canvas.js
+++ js/controllers/canvas.js
@@ -5,7 +5,7 @@ angularArt.controller('CanvasCtrl', function CanvasCtrl($scope) {
   $scope.canvas = { url: "http://www.psdgraphics.com/wp-content/uploads/2011/06/curved-road.jpg",
             color: "#0080c0", sepia: 0, invert: 0, saturation: 100, hue: 0, radialColor: "" };
   $scope.logo = { url: "http://ukartsdirectory.com/wp-content/uploads/2013/11/your-logo-here.png",
-          width: 250, positionX: -253, positionY: -38, rotate: 0, border: 0, scaleX: 1,
+          width: 250, positionX: -253, positionY: -40, rotate: 0, border: 0, scaleX: 1,
           scaleY: 1 };
   $scope.font = { type: "'Arial Black', Gadget, sans-serif", size: 52, positionX: -103, positionY: 164,
           text: "112-325-1321", color: "#000000", strokeColor: "#ffffff", strokeSize: 2,
