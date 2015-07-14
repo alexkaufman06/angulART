d254f20d51219e827a66f0eb7c3228dde0a3d1aa
Alex Kaufman <alexkaufman06@gmail.com>
2015-07-14 11:02:27 -0700

    Update canvas.js

--- js/controllers/canvas.js
+++ js/controllers/canvas.js
@@ -1,13 +1,13 @@
 angularArt.controller('CanvasCtrl', function CanvasCtrl($scope) {
-  $scope.imageOne = { url: "http://png-4.findicons.com/files/icons/1012/racing_cars/256/lamborghini.png",
-                    sepia: 0, invert: 0, rotate: 345, width: 153, positionX: 211, positionY: 189,
+  $scope.imageOne = { url: "http://vignette2.wikia.nocookie.net/imaginaughts/images/8/81/Purple-dragon.png/revision/latest?cb=20110909191420",
+                    sepia: 100, invert: 0, rotate: 357, width: 253, positionX: 253, positionY: 198,
                     scaleX: 1, scaleY: 1, border: 0 };
-  $scope.canvas = { url: "http://www.psdgraphics.com/wp-content/uploads/2011/06/curved-road.jpg",
-            color: "#0080c0", sepia: 0, invert: 0, saturation: 100, hue: 0, radialColor: "" };
-  $scope.logo = { url: "http://ukartsdirectory.com/wp-content/uploads/2013/11/your-logo-here.png",
-          width: 250, positionX: -253, positionY: -40, rotate: 0, border: 0, scaleX: 1,
+  $scope.canvas = { url: "", color: "#0080c0", sepia: 80, invert: 5,
+                    saturation: 826, hue: 0, radialColor: "#ff0000" };
+  $scope.logo = { url: "http://static.socialpoint.es/content/imgsxml/en/galeria_personajes/flame-.png",
+          width: 250, positionX: -297, positionY: 163, rotate: 0, border: 0, scaleX: 1,
           scaleY: 1 };
-  $scope.font = { type: "'Arial Black', Gadget, sans-serif", size: 52, positionX: -103, positionY: 164,
-          text: "112-325-1321", color: "#000000", strokeColor: "#ffffff", strokeSize: 2,
+  $scope.font = { type: "'Arial Black', Gadget, sans-serif", size: 52, positionX: 8, positionY: 129,
+          text: "By: Alex Kaufman", color: "#000000", strokeColor: "#ffffff", strokeSize: 2,
           rotate: 0 };
 });
