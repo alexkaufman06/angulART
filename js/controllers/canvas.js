35e11f6456efb6596fc6f012c3f8fac5bff76c65
Alex Kaufman <alexkaufman06@gmail.com>
2015-07-14 11:08:37 -0700

    Fix text and image

--- js/controllers/canvas.js
+++ js/controllers/canvas.js
@@ -1,8 +1,8 @@
 angularArt.controller('CanvasCtrl', function CanvasCtrl($scope) {
   $scope.imageOne = { url: "http://vignette2.wikia.nocookie.net/imaginaughts/images/8/81/Purple-dragon.png/revision/latest?cb=20110909191420",
-                    sepia: 100, invert: 0, rotate: 357, width: 253, positionX: 253, positionY: 198,
+                    sepia: 0, invert: 0, rotate: 357, width: 253, positionX: 253, positionY: 198,
                     scaleX: 1, scaleY: 1, border: 0 };
-  $scope.canvas = { url: "", color: "#0080c0", sepia: 80, invert: 5,
+  $scope.canvas = { url: "", color: "", sepia: 80, invert: 5,
                     saturation: 826, hue: 0, radialColor: "#ff0000" };
   $scope.logo = { url: "http://static.socialpoint.es/content/imgsxml/en/galeria_personajes/flame-.png",
           width: 250, positionX: -297, positionY: 163, rotate: 0, border: 0, scaleX: 1,
